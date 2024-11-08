665ae4faa464f5892a3cb18955a72e587b8c8244
Caytrades <nanakwameopokuababio@gmail.com>
2024-11-08 11:24:36 +0000

    Environment Setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import app from "./app"
+import { error } from "console";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGODB_URI || " ";
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Connnected to Database")
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+    }).catch((error) => console.error("MongoDB connection error:", error))
+
